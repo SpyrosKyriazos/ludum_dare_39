2c82970825c75939b35f04ec42a57a6f21b34809
Spyros <kyr.spyridon@gmail.com>
2017-07-30 19:42:00 +0300

    finished logic and graphics

--- project/scripts/base_script.gd
+++ project/scripts/base_script.gd
@@ -8,20 +8,21 @@ const image4 = preload("res://assets/dice4.png");
 const image5 = preload("res://assets/dice5.png");
 const image6 = preload("res://assets/dice6.png");
 const rounds = 20
-const ai_wait_time = 0.1
+const ai_wait_time = 2
 const ai_area1 = Vector2(240,250)
 const ai_area2 = Vector2(240,346)
 const ai_area3 = Vector2(240,442)
 const ai_area4 = Vector2(240,538)
-const c_approval_gain = 5
+const c_approval_gain = 6
 const c_approval_cost = 1
 const c_military_gain = 2
 const c_military_cost = 10
-const c_food_gain = 1
+const c_food_gain = 8
 const c_food_cost = 10
-const c_saboteur_gain = -20
+const c_saboteur_gain = -30
 const c_saboteur_cost = 5
-var ai_players = [1,2,3,4]
+var ai_players = global.ai_players
+var lost_players = []
 var round_num = 1
 var fam_step = 1
 var roll1 = 0
@@ -36,13 +37,13 @@ var dice3
 var dice4
 var dice5
 var dice6
-var fam1_points = 50
+var fam1_points = 500
 var fam1_sabotage = 0
-var fam2_points = 50
+var fam2_points = 500
 var fam2_sabotage = 0
-var fam3_points = 50
+var fam3_points = 500
 var fam3_sabotage = 0
-var fam4_points = 50
+var fam4_points = 500
 var fam4_sabotage = 0
 var approval_points = 0
 var military_points = 0
@@ -106,10 +107,10 @@ func populate_points():
 
 func populate_item_list():
 	var player_list = get_node("distribution/player_list")
-	player_list.add_item("P1 - Gillfield")
-	player_list.add_item("P2 - Heltook")
-	player_list.add_item("P3 - Augustham")
-	player_list.add_item("P4 - Rainmere")
+	player_list.add_item(" P1 - Gillfield")
+	player_list.add_item(" P2 - Heltook")
+	player_list.add_item(" P3 - Augustham")
+	player_list.add_item(" P4 - Rainmere")
 
 func _fixed_process(delta):
 	update_area_values()
@@ -170,37 +171,37 @@ func _on_roll_button_pressed():
 	get_node("roll/roll_button").set_disabled(true)
 	
 	dice1 = dice_scene.instance()
-	dice1.set_pos(Vector2(753, 440))
+	dice1.set_pos(Vector2(511, 440))
 	get_node("dices/all_dice").add_child(dice1)
 	set_dice_image(dice1, roll1)
 	dice1.value = roll1
 	
 	dice2 = dice_scene.instance()
-	dice2.set_pos(Vector2(801, 440))
+	dice2.set_pos(Vector2(559, 440))
 	get_node("dices/all_dice").add_child(dice2)
 	set_dice_image(dice2, roll2)
 	dice2.value = roll2
 	
 	dice3 = dice_scene.instance()
-	dice3.set_pos(Vector2(849, 440))
+	dice3.set_pos(Vector2(607, 440))
 	get_node("dices/all_dice").add_child(dice3)
 	set_dice_image(dice3, roll3)
 	dice3.value = roll3
 	
 	dice4 = dice_scene.instance()
-	dice4.set_pos(Vector2(897, 440))
+	dice4.set_pos(Vector2(655, 440))
 	get_node("dices/all_dice").add_child(dice4)
 	set_dice_image(dice4, roll4)
 	dice4.value = roll4
 	
 	dice5 = dice_scene.instance()
-	dice5.set_pos(Vector2(945, 440))
+	dice5.set_pos(Vector2(703, 440))
 	get_node("dices/all_dice").add_child(dice5)
 	set_dice_image(dice5, roll5)
 	dice5.value = roll5
 	
 	dice6 = dice_scene.instance()
-	dice6.set_pos(Vector2(993, 440))
+	dice6.set_pos(Vector2(751, 440))
 	get_node("dices/all_dice").add_child(dice6)
 	set_dice_image(dice6, roll6)
 	dice6.value = roll6
@@ -246,46 +247,72 @@ func _on_apply_button_pressed():
 	prepare_next_round()
 
 func prepare_next_round():
-	if fam_step < 4:
-		fam_step += 1
+	if lost_players.size() < 3:
+		var skip = true
+		while skip:
+			if fam_step < 4:
+				fam_step += 1
+			else:
+				round_num += 1
+				fam_step = 1
+			if !lost_players.has(fam_step):
+				skip = false
+		
+		reset_rolls()
+		get_node("rounds/round_value").set_text(str(round_num))
+		get_node("rounds/player_value").set_text(str(fam_step))
+		populate_distribution()
+		
+		if ai_players.has(fam_step):
+			ai_turn()
 	else:
-		round_num += 1
-		fam_step = 1
-#		calculate_saboteurs()
-	
-	reset_rolls()
-	get_node("rounds/round_value").set_text(str(round_num))
-	get_node("rounds/player_value").set_text(str(fam_step))
-	populate_distribution()
-	
-	if ai_players.has(fam_step):
-		ai_turn()
+		round_num = rounds
 	
 	if round_num == rounds:
 		# game over
 		ai_players.clear()
 		get_node("rounds/apply_button").set_disabled(true)
+		game_over()
+	
+#	if ai_players.has(fam_step):
+#		prepare_next_round()
+
+func check_player_over():
+	if fam_step == 1:
+		return fam1_points <= 0
+	elif fam_step == 2:
+		return fam2_points <= 0
+	elif fam_step == 3:
+		return fam3_points <= 0
+	elif fam_step == 4:
+		return fam4_points <= 0
 
 func ai_turn():
 	get_node("rounds/apply_button").set_disabled(true)
-	print("in ai: " + str(fam_step))
+#	print("in ai: " + str(fam_step))
+	# wait
+	var t = Timer.new()
+	t.set_wait_time(ai_wait_time/4)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
 	# roll
 	_on_roll_button_pressed()
 	# wait
-	var t = Timer.new()
-	t.set_wait_time(ai_wait_time)
-	t.set_one_shot(true)
-	self.add_child(t)
+	t.set_wait_time(ai_wait_time/2)
 	t.start()
 	yield(t, "timeout")
 	# assign values
 	ai_assing_values()
 	# wait
+	t.set_wait_time(ai_wait_time)
 	t.start()
 	yield(t, "timeout")
 	# apply 
-	_on_apply_button_pressed()
 	get_node("rounds/apply_button").set_disabled(false)
+	_on_apply_button_pressed()
+	
 
 func ai_assing_values():
 	var dice_used = []
@@ -294,12 +321,13 @@ func ai_assing_values():
 #	print(rolls)
 	
 	# check doubles/triples
-	var search = true
-	var tens_found = 0
 	var search_value = food_cost
 	for c in [0,1]:
 		if c == 1:
 			search_value = saboteur_cost
+		
+		var search = true
+		var tens_found = 0
 		
 		while search:
 			var found = false
@@ -308,7 +336,8 @@ func ai_assing_values():
 			var index3 = null
 			for i in range(rolls.size()):
 				if !dice_used.has(i):
-					if rolls[i] == search_value:
+#					print("i:"+str(i)+", roll:"+str(rolls[i])+", value:"+str(search_value)+", mod:"+str(rolls[i]%search_value))
+					if rolls[i]%search_value == 0:
 						index1 = i
 						found = true
 						break
@@ -316,7 +345,8 @@ func ai_assing_values():
 						for j in range(rolls.size()):
 							if j > i && !dice_used.has(j):
 								var sum = rolls[i]+rolls[j]
-								if sum == search_value:
+#								print("j:"+str(j)+", sum:"+str(sum)+", value:"+str(search_value)+", mod:"+str(sum%search_value))
+								if sum%search_value == 0:
 									index1 = i
 									index2 = j
 									found = true
@@ -325,7 +355,8 @@ func ai_assing_values():
 									for k in range(rolls.size()):
 										if k > j && !dice_used.has(k):
 											var sum = rolls[i]+rolls[j]+rolls[k]
-											if sum == search_value:
+#											print("k:"+str(k)+", sum:"+str(sum)+", value:"+str(search_value)+", mod:"+str(sum%search_value))
+											if sum%search_value == 0:
 												index1 = i
 												index2 = j
 												index3 = k
@@ -345,15 +376,20 @@ func ai_assing_values():
 				if index3 != null:
 					dice_used.append(index3)
 				
-				var pos = 3
-				if approval_gain > c_approval_gain*1.8:
+				var pos
+				if c == 1:
 					pos = 1
-				elif saboteur_cost < c_saboteur_cost/1.5:
-					pos = 4
-					var index = find_top_player(fam_step)
-					get_node("distribution/player_list").select(index,true)
-				elif tens_found > 1 && saboteur_cost >= 1:
-					pos = 2
+					if saboteur_cost <= c_saboteur_cost/1.25:
+						pos = 4
+						var player_index = find_top_player(fam_step)
+						get_node("distribution/player_list").select(player_index-1,true)
+				else:
+					pos = 3
+					if tens_found > 1 && saboteur_cost >= 2:
+						pos = 2
+					elif approval_gain >= c_approval_gain*2:
+						pos = 1
+				
 				move_dice(all_dice[index1], pos)
 				if index2 != null:
 					move_dice(all_dice[index2], pos)
@@ -389,14 +425,13 @@ func move_dice(dice, pos_index):
 
 func find_top_player(exclude):
 	var all_points = [fam1_points, fam2_points, fam3_points, fam4_points]
-	if exclude != null:
-		all_points.remove(exclude-1)
 	var this_max = -100000
 	var max_index
 	for i in range(all_points.size()):
 		if all_points[i]>this_max:
-			this_max = all_points[i]
-			max_index = i
+			if (exclude != null && exclude-1 != i) || exclude == null:
+				this_max = all_points[i]
+				max_index = i
 	return max_index + 1
 
 func populate_distribution():
@@ -453,6 +488,7 @@ func apply_points():
 	var total_military = int(military_points/military_cost)
 	approval_gain += total_foods
 	saboteur_cost -= total_military
+	if saboteur_cost < 1: saboteur_cost = 1
 	
 	var player_list = get_node("distribution/player_list")
 	var selected_array = player_list.get_selected_items()
@@ -462,15 +498,19 @@ func apply_points():
 	if player_index != null:
 		if player_index == 0:
 			fam1_points += sab_points
+			if fam1_points <=0: lost_players.append(1)
 			get_node("stats/points1").set_text(str(fam1_points))
 		elif player_index == 1:
 			fam2_points += sab_points
+			if fam2_points <=0: lost_players.append(2)
 			get_node("stats/points2").set_text(str(fam2_points))
 		elif player_index == 2:
 			fam3_points += sab_points
+			if fam3_points <=0: lost_players.append(3)
 			get_node("stats/points3").set_text(str(fam3_points))
 		elif player_index == 3:
 			fam4_points += sab_points
+			if fam4_points <=0: lost_players.append(4)
 			get_node("stats/points4").set_text(str(fam4_points))
 	
 	if fam_step == 1:
@@ -527,3 +567,27 @@ func wait(value):
 	self.add_child(t)
 	t.start()
 	yield(t, "timeout")
+
+func game_over():
+	var winner_text
+	var winner_index = find_top_player(null)
+	if winner_index == 1: winner_text = "P1 - The House of Gillfield"
+	elif winner_index == 2: winner_text = "P2 - The House of Heltook"
+	elif winner_index == 3: winner_text = "P3 - The House of Augustham"
+	elif winner_index == 4: winner_text = "P4 - The House of Rainmere"
+	winner_text += " is the most powerful in the land."
+	
+#	var dialog = get_node("game_over_dialog")
+#	dialog.set_text(winner_text + "\n\n Press OK to return to menu")
+#	dialog.popup()
+	
+	var panel = get_node("game_over_panel")
+	var message = get_node("game_over_panel/winner_label")
+	message.set_text(winner_text)
+	panel.popup()
+
+func _on_game_over_dialog_confirmed():
+	get_tree().change_scene("res://scenes/menu.tscn")
+
+func _on_back_to_menu_pressed():
+	get_tree().change_scene("res://scenes/menu.tscn")
---
+++ project/scripts/menu.gd
@@ -0,0 +1,39 @@
+extends Node
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	populate_item_lists()
+
+func populate_item_lists():
+	var list1 = get_node("option1/option_button1")
+	list1.add_item("Player")
+	list1.add_item("Computer")
+	list1.select(0)
+	
+	var list2 = get_node("option2/option_button1")
+	list2.add_item("Player")
+	list2.add_item("Computer")
+	list2.select(1)
+	
+	var list3 = get_node("option3/option_button1")
+	list3.add_item("Player")
+	list3.add_item("Computer")
+	list3.select(1)
+	
+	var list4 = get_node("option4/option_button1")
+	list4.add_item("Player")
+	list4.add_item("Computer")
+	list4.select(1)
+
+func _on_button_start_pressed():
+	global.ai_players.clear()
+	var option1 = get_node("option1/option_button1").get_selected()
+	if option1 == 1: global.ai_players.append(1)
+	var option2 = get_node("option2/option_button1").get_selected()
+	if option2 == 1: global.ai_players.append(2)
+	var option3 = get_node("option3/option_button1").get_selected()
+	if option3 == 1: global.ai_players.append(3)
+	var option4 = get_node("option4/option_button1").get_selected()
+	if option4 == 1: global.ai_players.append(4)
+	get_tree().change_scene("res://scenes/game_root.tscn")
